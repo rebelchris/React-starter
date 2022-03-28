c9c89f3759ec9a945dde2375425f6af27cc3e504
Chris Bongers <chrisbongers@gmail.com>
2022-03-28 08:08:41 +0200

    test: Added first basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders default text', () => {
+  render(<App />);
+  const defaultText = screen.getByText(/The counter is now on/g);
+  expect(defaultText).toBeInTheDocument();
+});
+
+test('should default to zero', () => {
+  render(<App />);
+  const defaultText = screen.getByText('The counter is now on 0');
+  expect(defaultText).toBeInTheDocument();
+});
+
+test('should update the count', async () => {
+  render(<App />);
+  const button = await screen.findByRole('button');
+  button.click();
+  const plusText = screen.getByText('The counter is now on 1');
+  expect(plusText).toBeInTheDocument();
 });
